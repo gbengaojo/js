80932a474b8c3d5b21f5d93d6a753602d9a22501
Gbenga Ojo <gbenga@gbengaojo.com>
2015-01-19 22:56:51 -0600

    begin request handler dependancy injection

--- nodebeginner.org/router.js
+++ nodebeginner.org/router.js
@@ -1,5 +1,11 @@
-function route(pathname) {
+function route(handle, pathname) {
    console.log("Routing request for " + pathname + " ...");
+
+   if (typeof handle[pathname] === 'function') {
+      handle[pathname]();
+   } else {
+      console.log("No request handler found for " + pathname);
+   }
 }
 
 exports.route = route;
