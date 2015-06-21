310d39485415bc1d180265c775972b04c01a0a71
Gbenga Ojo <gbenga@gbengaojo.com>
2015-06-21 18:43:09 -0500

    transitioning to show only complete todos

--- emberjs/guide/js/router.js
+++ emberjs/guide/js/router.js
@@ -2,7 +2,19 @@ Todos.Router.map(function() {
    this.resource('todos', { path: '/' }, function () {
       // additional child routes
       this.route('active');
+      this.route('completed');
    });
+});
+
+Todos.TodosCompletedRoute = Ember.Route.extend({
+   model: function () {
+      return this.store.filter('todo', function(todo) {
+         return todo.get('isCompleted');
+      });
+   },
+   renderTemplage: function(controller) {
+      this.render('todos/index', {controller: controller});
+   }
 });
 
 Todos.TodosActiveRoute = Ember.Route.extend({
