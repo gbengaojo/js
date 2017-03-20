ca893f043d88915c7d81abf0565c02cbb3da79c4
Gbenga Ojo <gbenga@gbengaojo.com>
2017-03-20 15:52:41 -0500

    shortcut keys

--- electron/sound-machine/main.js
+++ electron/sound-machine/main.js
@@ -1,1 +1,30 @@
+'use srtict';
 
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var ipc = require('ipc');
+var globalShortcut = require('global-shortcut');
+
+var mainWindow = null
+
+app.on('ready', function() {
+   mainWindow = new BrowserWindow({
+      frame: false,
+      height: 700,
+      resizable: false,
+      width: 368
+   });
+
+   mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
+
+   globalShortcut.register('ctrl+shift+1', function () {
+      mainWindow.webContents.send('global-shortcut', 0);
+   });
+   globalShortcut.register('ctrl+shift+2', function () {
+      mainWindow.webContents.send('global-shortcut', 1);
+   }); 
+});
+
+ipc.on('close-main-window', function () {
+   app.quit();
+});
