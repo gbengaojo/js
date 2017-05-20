c651867e9ea17b1d07b5c7120e33d96e9d87769b
Gbenga Ojo <gbenga@gbengaojo.com>
2017-05-20 07:54:24 -0500

    express lib challenge

--- electron/express-locallibrary-tutorial/routes/users.js
+++ electron/express-locallibrary-tutorial/routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+/* GET additional challenge. */
+router.get('/challenge', function(req, res, next) {
+	res.send('additional challenge');
+});
+
 module.exports = router;
